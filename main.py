74b9085f5ed2700957173449e61887eb900e20fa
ybressler <yaakov.bressler@ginger.io>
2023-10-26 21:57:11 -0700

    Add pandas an polars

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-
+# -*- coding: utf-8 -*-
 import duckdb
+
+con: duckdb.DuckDBPyConnection = duckdb.connect()
+
+def load_extension(con: duckdb.DuckDBPyConnection):
+    """Install and load extensi9ons"""
+    con.install_extension("spatial")
+    con.load_extension("spatial")
+
+
+def load_data(con: duckdb.DuckDBPyConnection):
+    """Load data from tables"""
+    # Load data
+    duckdb.sql("CREATE TABLE traffic_signals_csv AS SELECT  * FROM read_csv_auto('data/Traffic_Signals.csv')")
+    duckdb.sql("CREATE TABLE traffic_signals_json AS SELECT  * FROM read_json_auto('data/Traffic_Signals.geojson')")
 
 
 if __name__ == '__main__':
 
-    duckdb.sql('SELECT 42').show()
+    # Load extensions
+    load_extension(con)
+    load_data(con)
+
+    df = con.sql('SELECT * FROM traffic_signals').df()  # Converts to a pandas df
+    print(df.head())
