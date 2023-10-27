601cf174b511b87f61f02d85f9ac810b0a983601
ybressler <yaakov.bressler@ginger.io>
2023-10-26 22:17:07 -0700

    Setup DB with these commands.

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 # -*- coding: utf-8 -*-
 import duckdb
 
-con: duckdb.DuckDBPyConnection = duckdb.connect()
+con: duckdb.DuckDBPyConnection = duckdb.connect("my_db.db")
+
 
 def load_extension(con: duckdb.DuckDBPyConnection):
-    """Install and load extensi9ons"""
+    """Install and load extensions"""
     con.install_extension("spatial")
     con.load_extension("spatial")
 
 
-def load_data(con: duckdb.DuckDBPyConnection):
-    """Load data from tables"""
+def load_data(con: duckdb.DuckDBPyConnection, replace: bool = False):
+    """
+    Load data from compressed csv | geojson format.
+
+    Args:
+        con: Database connection
+        replace: Replace tables if they exist? (Default is no, makes things faster.)
+    """
     # Load data
-    duckdb.sql("CREATE TABLE traffic_signals_csv AS SELECT  * FROM read_csv_auto('data/Traffic_Signals.csv')")
-    duckdb.sql("CREATE TABLE traffic_signals_json AS SELECT  * FROM read_json_auto('data/Traffic_Signals.geojson')")
+    load_tables = [
+        ("corners", "Corners_Improved.csv.gz"),
+        ("curbs", "Curbs.csv.gz"),
+        ("signs", "Signs.csv.gz"),
+        ("traffic_signals", "Traffic_Signals.csv.gz"),
+    ]
+
+    if replace:
+        create_command = "CREATE OR REPLACE TABLE"
+    else:
+        create_command = "CREATE TABLE IF NOT EXISTS"
+
+    for table_name, file_name in load_tables:
+        file_path = f"data/{file_name}"
+        con.sql(f"{create_command} {table_name} AS SELECT  * FROM read_csv_auto('{file_path}')")
+
 
 
 if __name__ == '__main__':
@@ -22,5 +43,6 @@ if __name__ == '__main__':
     load_extension(con)
     load_data(con)
 
+    con.sql("SHOW ALL TABLES;").show()
     df = con.sql('SELECT * FROM traffic_signals').df()  # Converts to a pandas df
     print(df.head())
